087673f20fe5800cc017d471763bb28ed71d04ff
Gustaw Kuston <gustkust@gmail.com>
2022-05-05 10:02:13 +0200

    case dla cs_size = 1

--- ogrodnicy.c
+++ ogrodnicy.c
@@ -5,11 +5,14 @@
 #include <unistd.h>
 #include <time.h>
 #include <sys/time.h>
+#include <string.h>
+#include <pthread.h>
 
 #define ROOT 0
 
 int main(int argc, char **argv)
 {
+
     srand(time(NULL));
     int size, rank;
 
@@ -21,8 +24,68 @@ int main(int argc, char **argv)
     MPI_Status status;
     int res;
     int i;
+
+    int cs_queue[size - 1]; // indeks to rank procesu, wartosc to wartosc timestampu tego procesu,
+    // a miejce w kolejce to ilosc procesow z mniejszym timestampem + 1
+    memset(cs_queue, 9999, sizeof cs_queue);
+    int timestamps[size - 1];
+    memset(timestamps, 0, sizeof timestamps);
+
+    int ok = -1;
+    int no = -2;
+    int cs_size = 0;
+
+    int ok_num = 0; // ilosc otrzymanych ok
+    int got_no = 0; // czy dostalem no
+
+    int timestamp = 0;
+
+    void *listening(void *arg)
+    {
+        while (1)
+        {
+            printf("Jestem %d 1, czekam na wiadomość\n", rank);
+            MPI_Recv(&res, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            printf("Jestem %d 1, dostałem wiadomość %d od %d\n", rank, res, status.MPI_SOURCE);
+            sleep(1);
+            if (status.MPI_SOURCE != 0)
+            { // otrzymano timestamp
+                timestamps[status.MPI_SOURCE - 1] = res;
+                cs_queue[status.MPI_SOURCE - 1] = res;
+                if (res == ok)
+                {
+                    ok_num++;
+                    printf("Jestem %d 1, zwiększam licznik OK z %d do %d\n", rank, ok_num - 1, ok_num);
+                }
+                else if (res == no)
+                {
+                    got_no = 1;
+                    ok_num = 0;
+                    printf("Jestem %d 1, ustawiam, że dostałem NO\n", rank);
+                }
+                else if (res > timestamp || (res == timestamp && rank > status.MPI_SOURCE))
+                {
+                    printf("Jestem %d 1, wysyłam OK do %d\n", rank, status.MPI_SOURCE);
+                    timestamp = res;
+                    MPI_Send(&ok, 1, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD); // jak mam mniejszy to no
+                }
+                else if (res < timestamp || (res == timestamp && rank < status.MPI_SOURCE))
+                {
+                    printf("Jestem %d 1, wysyłam NO do %d\n", rank, status.MPI_SOURCE);
+                    MPI_Send(&no, 1, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD); // jak mam wiekszy to ok
+                }
+            }
+            else
+            {
+                printf("Jestem %d 1, zwiększam sobie rozmiar ścieżki krytycznej\n", rank);
+                cs_size++;
+            }
+        }
+    }
+
     if (rank == ROOT)
     { // instytut
+        sleep(1);
         int time_sleep = 5;
         int order_id;
         while (1)
@@ -42,104 +105,38 @@ int main(int argc, char **argv)
     }
     else
     {
-        int timestamp = rank;
-        int res = 0;
-        int no = -1;
-        int ok = 1;
-        MPI_Request request;
-        MPI_Comm comm;
+        pthread_t thread_id;
+        pthread_create(&thread_id, NULL, listening, NULL);
+        printf("Jestem %d 2, czekam\n", rank);
         while (1)
         {
-            printf("Jestem %d, czekam na wiadomosc\n", rank);
-            MPI_Recv(&res, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-            if (res < 5000) // jezeli res < 5000 to znaczy ze wiadomosc jest od innego ziomka i zawiera timestamp
+            if (cs_size > 0)
             {
-                printf("Jestem %d, otrzymałem wiadomosc z timestampem od %d\n", rank, status.MPI_SOURCE);
-                if (timestamp < res) // jezeli moj timestamp jest mniejszy to nie moze wejsc
+                sleep(1);
+                for (i = 1; i < size; i++)
                 {
-                    printf("Jestem %d, wysyłam nie do %d\n", rank, status.MPI_SOURCE);
-                    MPI_Isend(&no, 1, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD, &request);
+                    if (i != rank)
+                    {
+                        printf("Jestem %d 2, wysyłam prośbę o OK do %d\n", rank, i);
+                        MPI_Send(&timestamp, 1, MPI_INT, i, 0, MPI_COMM_WORLD); // pytam o ok
+                    }
                 }
-                else // jezeli moj timestamp jest wyzszy to spoko
+                while (1)
                 {
-                    printf("Jestem %d, wysyłam ok do %d\n", rank, status.MPI_SOURCE);
-                    MPI_Isend(&ok, 1, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD, &request);
+                    if (got_no == 1)
+                    {
+                        printf("Jestem %d 2, otrzymałem NO od %d\n", rank, status.MPI_SOURCE);
+                        cs_size = 0;
+                        break;
+                    }
+                    if (ok_num == size - 1 - cs_size)
+                    {
+                        printf("Jestem %d 2, otrzymałem OK od wszsytkich, wchodzę do sekcji krytycznej\n", rank);
+                        sleep(2);
+                        printf("Jestem %d 2, otrzymałem OK od wszsytkich, wychodzę z sekcji krytycznej\n", rank);
+                    }
                 }
             }
-            else // jezeli res >= 5000 to znaczy ze to zlecenie od instytutu
-            {
-                printf("Jestem %d, otrzymałem wiadomosc ze zleceniem\n", rank);
-                for (i = 1; i < size; i++) // wysylam prosbe o ok do kazdego ziomka
-                {
-                    if (i != rank) // poza soba samym
-                    {
-                        printf("Jestem %d, wysyłam prosbe o OK do %d\n", rank, i);
-                        MPI_Send(&timestamp, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
-                    }
-                }
-                int i = 0;
-                int got_no = 0;
-                while (i != size - 2) // czekam az dostane ok od kazdego ziomka poza soba samym i instytutem
-                {
-                    res = -2;
-                    MPI_Irecv(&res, 1, MPI_INT, i, MPI_ANY_TAG, MPI_COMM_WORLD, &request);
-                    if (res == no) // jak dostalem nie to lipa
-                    {
-                        printf("Jestem %d i dostałem NO\n", rank);
-                        got_no = 1;
-                    }
-                    else if (res == ok) // kal dostalem ok to zwiekszam licznik
-                    {
-                        i++;
-                    }
-                }
-                if (got_no == 0) // jak dostalem same ok to moge wbijac do sekcji
-                {
-                    printf("Jestem %d i jestem w sekcji krytycznej\n", rank);
-                }
-            }
-
-            //     while (1) {
-            //         // sleep(1);
-            //         MPI_Recv(&res, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-            //         for (i = 1; i < size; i++) { // sprawdzam czy ktos juz zdarzyl otrzymac zlecenie i pyta o OK
-            //             MPI_Irecv(&res, 1, MPI_INT, i, MPI_ANY_TAG, MPI_COMM_WORLD, &request);
-            //             if (res > 5000) break;
-            //         }
-            //         printf("Jestem %d i moj res to %d\n", rank, res);
-            //         if (res <= timestamp) { // jezeli jestem pierwszym co otrzymal zlecenie to res >= 5000 (warunek nie bedzie spelniony) w przeciwnym razie dostaje timestamp
-            //             printf("Jestem %d i wysyłam OK procesowi %d!\n", rank, i);
-            //             MPI_Send(&timestamp, 1, MPI_INT, i, 0, MPI_COMM_WORLD); // wysylam OK procesowi
-            //         } else if (res < 5000) { // jezeli moj timestamp jest mniejszy a res to nie zlecenie
-            //             printf("Jestem %d i wysyłam NIE procesowi %d!\n", rank, i);
-            //             MPI_Send(&no, 1, MPI_INT, i, 0, MPI_COMM_WORLD); // wysyłam NIE procesowi
-            //             break;
-            //         }
-            //     }
-
-            //     printf("Jestem %d i dostałem info o nowym zleceniu %d\n", rank, res);
-
-            //     for (i = 1; i < size; i++)
-            //     {
-            //         if (i != rank) {
-            //             printf("Jestem %d i wysyłam prośbę o OK do %d\n", rank, i);
-            //             MPI_Send(&timestamp, 1, MPI_INT, i, 0, MPI_COMM_WORLD); // wysylam kazdemy swoj timestamp
-            //         }
-            //     }
-            //     i = 1;
-            //     while (i != size - 1) { // -1 bo root
-            //         if (i != rank) {
-            //             printf("Jestem %d i czekam na OK od %d\n", rank, i);
-            //             MPI_Recv(&res, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status); // czekam na OK od każdego poza mna i rootem
-            //             printf("Jestem %d i dostałem OK od %d\n", rank, i);
-            //             if (timestamp < res) timestamp = res + 1; // update timestampu
-            //             if (res == -1) {
-            //                 printf("Jestem %d i dostałem NIE od %d\n", rank, i);
-            //             }
-            //         }
-            //         i++;
-            //     }
-            //     printf("Jestem %d i wchodzę do sekcji krytycznej!\n", rank);
         }
     }
     MPI_Finalize();
